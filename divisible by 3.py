4b56919008e397de44443e692d6e662eaeacab5a
Rashmi D C <rashmidc228@gmail.com>
2026-04-18 21:37:59 +0530

    Delete divisible by 3.py

--- divisible by 3.py
+++
@@ -1,7 +0,0 @@
-start=int(input("Enter the starting no.: "))
-end=int(input("Enter the ending no.: "))
-
-print(f"Numbers divisible by 3 from {start} to {end}: ")
-for num in range(start,end+1):
-    if num % 3==0:
-        print(num)
